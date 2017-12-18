6380980bd340fb86c01e026f87a32394b954a3d9
abigailao <abigail.ao@outlook.com>
2017-12-18 09:59:20 +0100

    Javascript Revised

---
+++ basisjavascript/basis/h01.js
@@ -0,0 +1,21 @@
+//Opdracht 1.1
+console.log("Hello World");
+
+//Opdracht 1.2
+var a = 10;
+if(a > 9){
+}
+
+for(var i = 0; i < 10; i++){
+    console.log("de waarde van i is " + i);
+}
+
+//Opdracht 1.3
+var mijnauto =  {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalwielen: 4,
+    kleur: "blauw"
+}
+
+console.log(mijnauto.kleur);
