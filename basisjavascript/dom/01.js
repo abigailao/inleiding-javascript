77e4c924ee3f710bbb1e79d2b2beea7e045b2307
abigailao <abigail.ao@outlook.com>
2017-12-18 09:59:48 +0100

    Javascript Revised

---
+++ basisjavascript/dom/01.js
@@ -0,0 +1,5 @@
+// // Opdracht 2.1
+document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+document.getElementById("subkop").innerText = "Mijn geweldige subkop";
+document.getElementById("almere1").src = "../../img/bk-almerebuiten.jpg";
+document.getElementById("almere2").src="../../img/bk-almerehaven.jpg";
---
+++ basisjavascript/dom/02.js
@@ -0,0 +1,19 @@
+// Opdracht 2.2
+//KLEUR BLOKJES
+document.getElementById("bl1").style.backgroundColor = "aqua";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
+
+//BLOKJES HOOGTE
+document.getElementById("bl2").style.marginTop = "20px";
+document.getElementById("bl3").style.marginTop = "60px";
+
+//BLOKJES TEKST
+document.getElementById("bl1").style.paddingTop = "20px";
+document.getElementById("bl1").style.paddingLeft = "20px";
+document.getElementById("bl2").style.paddingLeft = "20px";
+document.getElementById("bl3").style.paddingLeft = "40px";
+
+//BLOKJES PADDING
+document.getElementById()
